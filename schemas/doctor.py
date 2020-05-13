33c68ae31147655aadb12af078ce556aa7cdf725
José O <jose.mom.1304@gmail.com>
2020-05-13 16:47:36 -0430

    Adding validation to doctor

--- schemas/doctor.py
+++ schemas/doctor.py
@@ -1,5 +1,8 @@
 from ma import ma
+from marshmallow import validates, ValidationError
 from models.doctor import DoctorModel
+
+import datetime
 
 
 class DoctorSchema(ma.ModelSchema):
@@ -7,3 +10,16 @@ class DoctorSchema(ma.ModelSchema):
         model = DoctorModel
         load_only = ("password",)
         dump_only = ("id","created_at","updated_at","active",)
+
+    @validates("password")
+    def validate_password(self, value):
+        if len(value) < 8 or len(value) > 16:
+            raise ValidationError("Password length must be between 8 and 16 characters")
+
+    @validates("birth_date")
+    def validate_birth_date(self, value):
+        current_date = datetime.date.today()
+        age = (current_date - value).days / 365.25
+        if age < 18:
+            raise ValidationError("Doctor must be at least 18 years old")
+
